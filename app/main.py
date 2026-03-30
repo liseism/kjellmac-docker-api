e16df4d91a7f6a8f5062b1bb793ba15c2dfce24b
liseism <lisekjellman@gmail.com>
2026-03-30 10:01:55 +0300

    add ip

--- app/main.py
+++ app/main.py
@@ -10,3 +10,7 @@ def read_root():
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
     return {"id": id, "q": q}
+
+@app.get("/api/ip")
+def ip(request: Request):
+        return{"ip: request.client.host"}
