8924cdc92e66b051c537c08084e22be5424061fd
liseism <lisekjellman@gmail.com>
2026-04-22 09:19:45 +0300

    testtt

---
+++ app/db.py
@@ -0,0 +1,61 @@
+import os, psycopg
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_conn():
+    return psycopg.connect(DATABASE_URL, autocommit=True, row_factory=psycopg.rows.dict_row)
+
+def create_schema():
+    with get_conn() as conn, conn.cursor() as cur:
+        # Create the schema
+        cur.execute("""
+                    
+            -- Lägg till pgcrypto 
+            CREATE EXTENSION IF NOT EXISTS pgcrypto;
+                    
+            -----------
+            -- rooms
+            -----------
+            CREATE TABLE IF NOT EXISTS rooms (
+                id SERIAL PRIMARY KEY,
+                room_number INT NOT NULL,
+                created_at TIMESTAMP DEFAULT now()
+            );       
+            -- add columns
+            ALTER TABLE rooms ADD COLUMN IF NOT EXISTS room_type VARCHAR;
+            ALTER TABLE rooms ADD COLUMN IF NOT EXISTS price NUMERIC;
+
+            -----------
+            -- guests
+            -----------
+            CREATE TABLE IF NOT EXISTS guests (
+                id SERIAL PRIMARY KEY,
+                firstname VARCHAR NOT NULL,
+                lastname VARCHAR NOT NULL,
+                created_at TIMESTAMP DEFAULT now(),
+                address VARCHAR
+            );
+            ALTER TABLE guests ADD COLUMN IF NOT EXISTS api_key VARCHAR DEFAULT encode(gen_random_bytes(32), 'hex');
+                    
+            -----------
+            -- bookings
+            -----------    
+            CREATE TABLE IF NOT EXISTS bookings (
+                id SERIAL PRIMARY KEY,
+                guest_id INT REFERENCES guests(id), -- foreign key (främmande nyckel)
+                room_id INT REFERENCES rooms(id),
+                datefrom DATE DEFAULT now(),
+                dateto DATE,
+                info VARCHAR
+            );
+            ALTER TABLE bookings ADD COLUMN IF NOT EXISTS stars INT;
+                    
+            -- man kan sätta default också senare:
+            -- ALTER TABLE bookings ALTER COLUMN datefrom SET DEFAULT now();
+            -- lägg till främmande nyckel senare:
+            -- ALTER TABLE bookings ADD CONSTRAINT guest_id_key FOREIGN KEY (guest_id) REFERENCES guests(id);
+
+
+
+        """)
+
--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import APIKeyHeader
+from pydantic import BaseModel
+from datetime import date
+from app.db import get_conn, create_schema
 
+# OBS: Lektion 8, API kräver nu API-nyckel
 app = FastAPI()
 
-origins = ["*"]
+origins = ["*"] # Change to the real front end origin in production
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,17 +19,135 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+create_schema()
 
+api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
+
+def validate_api_key(api_key: str = Depends(api_key_header)):
+    if not api_key:
+        raise HTTPException(status_code=401, detail={"error": "API key missing"})
+    
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT * FROM guests WHERE api_key = %s
+        """, [api_key])
+        guest = cur.fetchone()
+        if not guest:
+            raise HTTPException(status_code=401, detail={"error": "Bad API Key"})
+        return guest
+        
+
+# datamodell för bokning
+class Booking(BaseModel):
+    guest_id: int
+    room_id: int
+    datefrom: date 
+    dateto: date
+
+# Main route for this API
 @app.get("/")
-def read_root():
-    return { "msg": "web hooked :-)", "v": "0.2" }
+def read_root(): 
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("SELECT version() ")
+        result = cur.fetchone()
+
+    return { "msg": f"Hotel API!", "db_status": result }
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+# List all guests 
+@app.get("/guests")
+def get_guests(guest: dict = Depends(validate_api_key)): 
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT g.*,
+                (SELECT count(*) 
+                    FROM bookings
+                    WHERE guest_id = g.id
+                        AND dateto < now()) AS prev_visits
+            FROM guests g
+            WHERE id = %s
+            ORDER BY g.lastname
+        """, [guest['id']])
+        guests = cur.fetchall()
+    return guests
+        
+    
+# List all rooms 
+@app.get("/rooms")
+def get_rooms(): 
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT * 
+            FROM rooms
+            ORDER BY room_number
+        """)
+        rooms = cur.fetchall()
+    return rooms
 
-@app.get("/api/ip")
-def ip(request: Request):
-        return{"ip: " + request.client.host}
+# Get one room
+@app.get("/rooms/{id}")
+def get_room(id: int):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT * 
+            FROM rooms 
+            WHERE id = %s
+        """, [id]) # list här, tuple används också
+        room = cur.fetchone()
+    return room      
+
+
+# List all bookings 
+@app.get("/bookings")
+def get_bookings(guest: dict = Depends(validate_api_key)): 
+    print(guest)
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT 
+                r.room_number,
+                g.firstname || ' ' || g.lastname AS guest_name,
+                (b.dateto - b.datefrom) AS nights,
+                r.price as price_per_night,
+                CASE 
+                    WHEN (b.dateto - b.datefrom) >= 7 THEN 
+                        -- 20 percent discount
+                        (b.dateto - b.datefrom) * r.price * 0.8
+                    ELSE (b.dateto - b.datefrom) * r.price
+                END as total_price,
+                b.*
+            FROM bookings b
+            INNER JOIN rooms r
+                ON r.id = b.room_id
+            INNER JOIN guests g
+                ON g.id = b.guest_id
+            WHERE b.guest_id = %s
+            ORDER BY id
+        """, [guest['id']] )
+        bookings = cur.fetchall()
+    return bookings
+
+# Create booking
+@app.post("/bookings")
+def create_booking(booking: Booking):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO bookings (
+                guest_id,
+                room_id,
+                datefrom,
+                dateto
+            ) VALUES (
+                %s, %s, %s, %s
+            ) RETURNING id
+        """, (
+            booking.guest_id, 
+            booking.room_id,
+            booking.datefrom,
+            booking.dateto
+        ))
+        new_booking = cur.fetchone()
+    return { "msg": "Booking created!", "id": new_booking['id']}
+
+
+
 
